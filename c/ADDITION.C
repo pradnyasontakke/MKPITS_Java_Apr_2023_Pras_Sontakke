dc146898606b82125733c09c22c5b24267da080f
pradnya sontakke <pradnyasontakke14@gmail.com>
2023-05-11 11:21:39 +0530

    pradnya

---
+++ c/ADDITION.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num1,num2,res;
+printf("enter rhe two numbersfor the addition");
+scanf(%d%d &num1,&num2);
+res= num1+num2;
+printf(" result = %d", res);
+get ch();
+
+}
